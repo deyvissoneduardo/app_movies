8894311871b8741fcf100c5cad255af9cfeefe92
DeyvissonEduardo <deyvissoneduardo22@gmail.com>
2023-04-20 17:03:38 -0300

    layouts

--- lib/modules/movie_details/movie_details_controller.dart
+++ lib/modules/movie_details/movie_details_controller.dart
@@ -1,3 +1,26 @@
+import 'package:app_movies/core/rest_client/movie_api.dart';
+import 'package:app_movies/models/movie_details_model.dart';
 import 'package:get/get.dart';
 
-class MovieDetailsController extends GetxController {}
+class MovieDetailsController extends GetxController {
+   int movieId = Get.arguments[0];
+  MovieApi movieApi = MovieApi();
+  
+  bool isLoading = true;
+  List<MovieDetailsModel> movieData = [];
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchMovieDetails();
+  }
+
+  void fetchMovieDetails() async {
+    var response = await movieApi.getMovieDetails(movieId: movieId);
+    if(response != null){
+      movieData.add(MovieDetailsModel.fromMap(response));
+      isLoading = false;
+      update();
+    }
+  }
+}
--- lib/modules/upcoming_movie/upcoming_movie_controller.dart
+++ lib/modules/upcoming_movie/upcoming_movie_controller.dart
@@ -1,3 +1,26 @@
+import 'package:app_movies/core/rest_client/movie_api.dart';
+import 'package:app_movies/models/upcoming_movie_model.dart';
 import 'package:get/get.dart';
 
-class UpcomingMovieController extends GetxController {}
+class UpcomingMovieController extends GetxController {
+  MovieApi movieApi = MovieApi();
+  List<UpcomingMovieModel> upcomingMovies = [];
+  bool isLoading = true;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchUpcomingMovies();
+  }
+
+  void fetchUpcomingMovies() async {
+    var response = await movieApi.getUpcomingMovies();
+    if (response != null) {
+      response['results'].forEach((data) {
+        upcomingMovies.add(UpcomingMovieModel.fromMap(data));
+      });
+      isLoading = false;
+      update();
+    }
+  }
+}
